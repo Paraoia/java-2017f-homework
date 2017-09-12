90525201021002829e4b65f5a0e6823798a8e7c4
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2017-09-12 16:53:29 +0800

    SortTask 0.1
    151220114
    王宇鑫

--- 20170912/151220114 王宇鑫/SortTest0.java
+++ 20170912/151220114 王宇鑫/SortTest0.java
@@ -16,7 +16,7 @@ public class SortTest0
     {
         List<Integer> nums = new ArrayList<>();
         Arrays.asList(args).forEach((String str) -> nums.add(new Integer(str)));
-        if (nums.isEmpty()) return Arrays.asList(new Integer[]{13, 28, 41, 9, 12, 50, 60});
+        if (nums.isEmpty()) return Arrays.asList(new Integer[]{32, 43, 53, 54, 32, 65, 63, 98, 43, 23});
         else return nums;
     }
 }
