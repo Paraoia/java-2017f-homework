88f425cec4175dfd5e65ddef4e95021f05f4c0ea
Minhui Xie <xiemhemail@gmail.com>
2017-09-12 22:13:21 +0800

    hw1

--- 20170912/谢旻晖-151220131/Hw1.java
+++ 20170912/谢旻晖-151220131/Hw1.java
@@ -4,23 +4,20 @@
 * @time 8:59 PM
 */
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Hw1 {
     public static void main(String[] args) {
-        Hw1 hw1=new Hw1();
-        hw1.showArray(hw1.sort(new int[]{1,2,5,56,3,4,5}));
-    }
-
-    public int[] sort(int [] list) {
-        Arrays.sort(list);
-        return list;
-    }
-    public void showArray(int [] list){
-        for (int it:list) {
-            System.out.print(""+it+' ');
+        Hw1 hw1 = new Hw1();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            list.add(sc.nextInt());
         }
+        Collections.sort(list);
+        System.out.println(list);
     }
 
 }
