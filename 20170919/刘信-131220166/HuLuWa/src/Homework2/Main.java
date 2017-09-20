78ef5c31e2bb56754bc63dfd780cdf8e38f0db9b
Lavender <yumehanabi@foxmail.com>
2017-09-20 21:19:26 +0800

    HomeWork #2 Run Main.java

---
+++ 20170919/刘信-131220166/HuLuWa/src/Homework2/Main.java
@@ -0,0 +1,13 @@
+package Homework2;
+
+public class Main {
+	public static void main(String [] Args){
+		Huluwas a = new Huluwas();
+		a.Disorganize();
+		a.BubbleSort();
+		a.SayName();
+		a.Disorganize();
+		a.BinarySort();
+		a.SayColor();
+	}
+}
