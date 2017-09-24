1b2299e25b587b349a63ebe828e52a6fda788e2a
liu <liu@triumphal.cn>
2017-09-24 22:28:12 +0800

    第二次作业

---
+++ 20170919/刘鎏-151220063/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        HuLuWa huluwa = new HuLuWa();
+        huluwa.randStand(true);
+        huluwa.bubbleSort();
+        huluwa.randStand(false);
+        huluwa.quickSort();
+    }
+}
