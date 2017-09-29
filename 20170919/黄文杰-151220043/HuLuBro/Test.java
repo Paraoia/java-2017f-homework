b8099fcbc6a4ad57113754225862ceedf95b0ac6
HuangWenJ <790028493@qq.com>
2017-09-29 10:27:52 +0800

    第二次作业修改版

--- 20170919/黄文杰-151220043/HuLuBro/Test.java
+++ 20170919/黄文杰-151220043/HuLuBro/Test.java
@@ -2,22 +2,33 @@ package HuLuBro;
 
 public class Test {
 	public static void main(String []args){
-		HuLuFamily hlf=new HuLuFamily();
-		Input input=new Input();
-		input.MessUp(hlf);
-		Output output=new Output();
-		output.printName(hlf);
-		System.out.println("");
-		Sort sort=new Sort();
-		sort.BubbleSort(hlf);
-		output.printName(hlf);
+		HuLuWa huluwa[]=new HuLuWa[7];
 		
-		System.out.println("");
-		input.MessUp(hlf);
-		output.printName(hlf);
-		System.out.println("");
-		sort.DivideSort(hlf);
-		output.printColor(hlf);
 		
+		huluwa[0]=new HuLuWa("老大",Color.RED,1);
+		huluwa[1]=new HuLuWa("老二",Color.ORANGE,2);
+		huluwa[2]=new HuLuWa("老三",Color.YELLOW,3);
+		huluwa[3]=new HuLuWa("老四",Color.GREEN,4);
+		huluwa[4]=new HuLuWa("老五",Color.CHING,5);
+		huluwa[5]=new HuLuWa("老六",Color.BLUE,6);
+		huluwa[6]=new HuLuWa("老七",Color.PURPLE,7);
+		
+		Home home=new Home(huluwa);
+		System.out.println("排序前的顺序:");
+		home.outName();
+		System.out.println("排序中...");
+		home.bubbleSort();
+		System.out.println("排序后的顺序:");
+		home.outName();
+		
+		home.messUp();
+		
+		System.out.println("排序前的顺序:");
+		home.outColor();
+		System.out.println("排序中...");
+		home.divideSort();
+		System.out.println("排序后的顺序:");
+		home.outColor();
+
 	}
 }
