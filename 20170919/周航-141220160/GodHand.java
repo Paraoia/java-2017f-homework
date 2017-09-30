99bbf1d8c3a54aee8a09753759e77df7473e76a2
njuzh <383395862@qq.com>
2017-09-30 06:30:29 -0700

    my homework 2 version 2nd

--- 20170919/周航-141220160/GodHand.java
+++ 20170919/周航-141220160/GodHand.java
@@ -2,28 +2,23 @@ import sun.reflect.generics.tree.BottomSignature;
 
 public class GodHand {
     public static void main(String[] args){
-        HuluSort hulu=new HuluSort();
+        Queue huluQueue=new Queue();
 
-        //初始化
-       // System.out.println("随机排列前：");
-        hulu.init();
-        //hulu.printName();
-        //随机站队
         System.out.print("随机排列后,");
-        hulu.randomQ();
-        hulu.printName();
+        huluQueue.randomQ();
+        huluQueue.printName();
         //冒泡排序
         System.out.println("冒泡排序：");
-        hulu.bubbleSort();
-        hulu.printName();
+        huluQueue.bubbleSort();
+        huluQueue.printName();
         //随机站队
         System.out.print("随机排列后,");
-        hulu.randomQ();
-        hulu.printName();
+        huluQueue.randomQ();
+        huluQueue.printName();
         //快速排序
         System.out.println("快速排序：");
-        hulu.quickSort(0,6);
+        huluQueue.quickSort(0,6);
         System.out.println(" ");
-        hulu.printColor();
+        huluQueue.printColor();
     }
 }
