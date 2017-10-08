8c833818956962213baee018a465a4012b7b4ecf
zfhsandra <zfhsandra@163.com>
2017-10-08 23:04:10 +0800

    作业提交

--- 20170919/赵芳鹤-141160079/huluwa.java
+++ 20170919/赵芳鹤-141160079/huluwa.java
@@ -1,12 +1,15 @@
 
 public class huluwa{
+    //葫芦娃按次序的年龄、颜色
     static private String ageSequence[] = {"老大","老二","老三","老四","老五","老六","老七"};
     static private String color[] = {"赤","橙","黄","绿","青","蓝","紫"};
 
     static public void printAgeSequence(int i){
+        //打印葫芦娃的年龄
         System.out.printf("%s",ageSequence[i]);
     }
     static public void printColor(int i){
+        //打印葫芦娃的颜色
         System.out.printf("%s",color[i]);
     }
 }
