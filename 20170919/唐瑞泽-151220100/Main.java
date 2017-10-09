49af17fff382b05b812377c80efa1fceb31273a2
Tang Ruize <1466040111@qq.com>
2017-10-09 21:47:18 +0800

    唐瑞泽-151220100 葫芦娃修改版

--- 20170919/唐瑞泽-151220100/Main.java
+++ 20170919/唐瑞泽-151220100/Main.java
@@ -1,30 +1,21 @@
-/**
- * Created by tangruize on 17-9-20.
- */
 public class Main {
-    public static void main(String[] args) {
-        FuluwaSort sort = new FuluwaSort();
-        Fuluwa []wa = new Fuluwa[7];
-        for (int i = 0; i < 7; ++i) {
-            wa[i] = new Fuluwa();
-        }
-        //Step 1: bubbleSort
-        sort.bubbleSort(wa);
-        for (int i = 0; i < 7; ++i) {
-            System.out.println(wa[i].getName());
-        }
-        //go home, fuluwa
-        for (int i = 0; i < 7; ++i) {
-            wa[i].goHome();
-        }
-        //come out, fuluwa!
-        for (int i = 0; i < 7; ++i) {
-            wa[i] = new Fuluwa();
-        }
-        //Step 2: quickSort
-        sort.quickSort(wa);
-        for (int i = 0; i < 7; ++i) {
-            System.out.println(wa[i].getColor());
-        }
+    public static void main(String []args) {
+        HuluwaSort huluwaSort = new HuluwaSort();
+
+        System.out.println("----Bubble sort start----");
+        huluwaSort.disorganize();
+        huluwaSort.bubbleSort();
+        System.out.println("----Bubble sort end----\n");
+        System.out.println("----Report name----");
+        huluwaSort.reportName();
+
+
+        System.out.println("\n----Quick sort start----");
+        huluwaSort.disorganize();
+        huluwaSort.quickSort();
+        System.out.println("----Quick sort end----\n");
+        System.out.println("----Report color----");
+        huluwaSort.reportColor();
+
     }
 }
