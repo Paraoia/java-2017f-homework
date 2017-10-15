95ec69d998c44071ed02eb402c02b96c60557d4e
LeBw <27091925@qq.com>
2017-10-16 01:39:52 +0800

    第三次作业提交

---
+++ 20171010/李博文-151220048/Formation.java
@@ -0,0 +1,3 @@
+public enum Formation {
+    鹤立, 雁行, 冲轭, 长蛇, 鱼鳞, 方门, 偃月, 锋矢
+}
---
+++ 20171010/李博文-151220048/Scorpion.java
@@ -0,0 +1,20 @@
+public class Scorpion implements Creature{
+    private Position position;
+    private String name = "蝎";
+    @Override
+    public void report() {
+        System.out.print(name);
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        this.position = position;
+        if (this.position != null)
+            this.position.setHolder(this);
+    }
+
+    @Override
+    public Position getPosition() {
+        return position;
+    }
+}
