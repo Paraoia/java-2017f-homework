0b3fb3b32e5c469cb524060d118b941901e8ea78
nimrodzhang <925688953@qq.com>
2017-11-22 16:14:12 +0800

    v3.1 with Colletions & Generics

--- 20171010/张书瀚-151220161/SquareFormatter.java
+++ 20171010/张书瀚-151220161/SquareFormatter.java
@@ -1,43 +1,46 @@
+import java.util.ArrayList;
+
 public class SquareFormatter implements Formatter, Constants {
     @Override
     public void format(Space space) {
         System.out.println("方円阵：");
 
-        Creature creatures[][] = space.getCreatures();
+        ArrayList<Creature> scorpion = space.getCreatures().get(3);
+        ArrayList<Creature> goblins = space.getCreatures().get(4);
         Position positions[][] = space.getPositions();
 
         for (int m = HALFSPACE; m < MAXSPACE; m++) {
-            for (int n = 0; n < MAXSPACE; n++) {
+            for (int n = 0; n < HALFSPACE; n++) {
                 positions[m][n].clearOccupy();
             }
         }
 
         //蝎子精位置
-        positions[12][4].setHolder(creatures[3][0]);
+        positions[12][4].setHolder(scorpion.get(0));
         positions[12][4].setOccupy();
-        creatures[3][0].setPos(positions[12][4]);
+        scorpion.get(0).setPos(positions[12][4]);
 
         //喽啰位置
         int k = 0;
         for (int i = 13, j = 3; i < 16; i++, j--, k++) {
-            positions[i][j].setHolder(creatures[4][k]);
+            positions[i][j].setHolder(goblins.get(k));
             positions[i][j].setOccupy();
-            creatures[4][k].setPos(positions[i][j]);
+            goblins.get(k).setPos(positions[i][j]);
         }
         for (int i = 13, j = 5; i < 16; i++, j++, k++) {
-            positions[i][j].setHolder(creatures[4][k]);
+            positions[i][j].setHolder(goblins.get(k));
             positions[i][j].setOccupy();
-            creatures[4][k].setPos(positions[i][j]);
+            goblins.get(k).setPos(positions[i][j]);
         }
         for (int i = 16, j = 2; i < 19; i++, j++, k++) {
-            positions[i][j].setHolder(creatures[4][k]);
+            positions[i][j].setHolder(goblins.get(k));
             positions[i][j].setOccupy();
-            creatures[4][k].setPos(positions[i][j]);
+            goblins.get(k).setPos(positions[i][j]);
         }
         for (int i = 16, j = 6; i < 18; i++, j--, k++) {
-            positions[i][j].setHolder(creatures[4][k]);
+            positions[i][j].setHolder(goblins.get(k));
             positions[i][j].setOccupy();
-            creatures[4][k].setPos(positions[i][j]);
+            goblins.get(k).setPos(positions[i][j]);
         }
     }
 }
